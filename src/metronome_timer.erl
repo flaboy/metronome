a4633da26cd59fc1e7a41877ce4adfbb64dbb381
Wanglei <flaboy.cn@gmail.com>
2013-02-21 19:15:52 +0800

    now(str)

--- src/metronome_timer.erl
+++ src/metronome_timer.erl
@@ -1,5 +1,5 @@
 -module(metronome_timer).
--export([start_link/0, init/0, now/0]).
+-export([start_link/0, init/0, now/0, now/1]).
 
 start_link()->
     Pid = spawn_link(?MODULE, init,[]),
@@ -13,6 +13,7 @@ loop()->
     {M,S,_} = erlang:now(),
     N = M*1000*1000 + S,
     ets:insert(metronome, {now, N}),
+    ets:insert(metronome, {now_str, time_now_str()}),
     timer:sleep(1000),
     loop().
     
@@ -21,3 +22,23 @@ now()->
         [{now,N}] -> N;
         _ -> 0
     end.
+
+now(str)->
+    case ets:lookup(metronome, now_str) of
+        [{now,N}] -> N;
+        _ -> time_now_str()
+    end.
+
+time_now_str()->
+    {{Y,M, D}, {H,I,S}} = calendar:local_time(),
+    Wdy = week(calendar:day_of_the_week({Y, M, D})),
+    io_lib:format("~s ~2.10.0B-~2.10.0B-~4.10.0B ~2.10.0B:~2.10.0B:~2.10.0B GMT", 
+        [Wdy, D, M, Y, H, I, S]).
+
+week(1)-> <<"Mon">>;
+week(2)-> <<"Tur">>;
+week(3)-> <<"Wed">>;
+week(4)-> <<"Thu">>;
+week(5)-> <<"Fri">>;
+week(6)-> <<"Sat">>;
+week(0)-> <<"Sun">>.
